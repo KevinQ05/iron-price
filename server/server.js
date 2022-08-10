7efcfdbaea51eb2a9c3d2cc3a11aa690ce54d72c
KevinQ05 <00131719@uca.edu.sv>
2022-08-09 20:51:35 -0600

    Initial Auth Setup

--- server/server.js
+++ server/server.js
@@ -1,14 +1,50 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
+const colors = require("colors");
+const errorHandler = require("./middleware/error");
+const cookieParser = require("cookie-parser");
 
-// Load environment variables
+//Load env vars
 dotenv.config({ path: "./config/config.env" });
+
+// Connect to database
+connectDB();
+
+//Route files
+const auth = require("./routes/auth");
 
 const app = express();
 
+// Body parser
+app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+//Mount routers
+app.use("/api/v1/auth", auth);
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
