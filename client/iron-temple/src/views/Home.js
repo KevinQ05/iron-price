0a3591acea655c8822216faf4a23d7609c4937af
000050519 <chad.0x1@protonmail.com>
2022-08-18 15:31:30 -0600

    Add: typography font
    added roboto mono font to project, its recommended font from material devs
    
    mrktman

--- client/iron-temple/src/views/Home.js
+++ client/iron-temple/src/views/Home.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Typography } from '@mui/material'
 
 const Home = () => {
   return (
     <div>
-      
+      <Typography> 
+       <h1> IRON FUK DEV TEAM WORKING YEAHBUDI</h1>
+      </Typography>
+      <h1>IRON FUK DEV TEAM WORKING</h1>
+
     </div>
   )
 }
